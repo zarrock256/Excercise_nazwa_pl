656ec0c1a7f89f4fbec9934f4d78398fbf1aed75
zarrock256 <61661076+zarrock256@users.noreply.github.com>
2022-04-03 22:01:47 +0200

    Change of main directory

--- Excersise_NazwaPl/src/main/java/Pages/CodeVerificationPage.java
+++
@@ -1,9 +0,0 @@
-package Pages;
-
-import org.openqa.selenium.By;
-
-public class CodeVerificationPage {
-    public static final By CODE_NUMBER = By.xpath("//span[@class=\"codeNumber\"]");
-    public static final By CODE_INPUT = By.xpath("//input[@class=\"authenticationCode\"]");
-    public static final By LOG_IN = By.xpath("//input[@class=\"na-button submit submitLogin\"]");
-}
--- Excersise_NazwaPl/src/main/java/Pages/MainPage.java
+++
@@ -1,8 +0,0 @@
-package Pages;
-
-import org.openqa.selenium.By;
-
-public class MainPage {
-    public static By CLIENTS_PANEL = By.xpath("//a[contains(text(), \"Panel Klienta\")]");
-    public static String CLIENTS_NICK = "//span[contains(text(), \"%s\")]";
-}
